bcccee79eee55671108ffa297d86845ecf115f6f
giri6200 <giriprasad6200@gmail.com>
2019-12-13 21:39:19 +0530

    1.Smoke test

---
+++ contracts/election.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.12;
+
+contract Election{
+	string public candidate;
+	//Constructor
+	constructor() public{
+		candidate="Candidate1";
+
+	}
+	//Reda constructor
+	//Store Candidate
+} 
